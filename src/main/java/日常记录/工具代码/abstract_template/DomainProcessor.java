5777da7710b2bde577852f9cef38256c82e53834
张利君 <kuake@youzan.com>
2019-01-23 20:28:27 +0800

    processor模板添加

---
+++ src/main/java/日常记录/工具代码/abstract_template/DomainProcessor.java
@@ -0,0 +1,17 @@
+package 日常记录.工具代码.abstract_template;
+
+/**
+ * 业务处理方法
+ *
+ * @author 夸克
+ * @date 2019/1/23 19:43
+ */
+public interface DomainProcessor<T> {
+
+    /**
+     * 处理业务的方法 返回值现在是void的 也可以自己定义一个通用的类似于上下文的返回值
+     *
+     * @param context
+     */
+    void process(T context);
+}
